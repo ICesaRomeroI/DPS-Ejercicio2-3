e2e6494f08a3503504175f53625bd3e84febc0f6
ICesaRomeroI <142021728+ICesaRomeroI@users.noreply.github.com>
2023-09-02 18:46:43 -0600

    Add files via upload

---
+++ ejercicio3/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () =>{
+
+    return(
+        <header>
+            <img src="https://cdn.cookielaw.org/logos/4db2f706-ab11-413a-898c-222ecb0b0a52/98e6d462-6db9-49cb-9ae5-d2e7166b4000/ae9c7c36-07b1-4fd6-8430-11d68eca6c70/tw_logo_horizontal_2.png" 
+            alt="Pizza" />
+        </header>
+    );
+}
+
+export default Header;
